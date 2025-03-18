96d7cd29223dcaa231278f8afb4391c4af204801
nik <nameissubjecttochange@gmail.com>
2025-03-18 18:38:06 -0400

    adjusted basic timer for further usecase

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-const start_mins = 10;
-let time = start_mins * 60;
-
+const button = document.getElementById('button');
 const countdown = document.getElementById('countdown');
-setInterval(update_countdown, 1000);
-function update_countdown() {
-  const mins = Math.floor(time / 60);
-  let secs = time % 60;
+const stop = document.getElementById('stop');
 
-  countdown.innerHTML = `${mins}: ${secs}`;
-  time--;
-}
+let timeleft = 1500;
+let interval;
+
+const update_countdown = () => {
+  const mins = Math.floor(timeleft / 60);
+  const secs = timeleft % 60;
+
+  countdown.innerHTML = `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2,'0')}`;
+};
+
+const start_countdown = () => {
+  interval = setInterval(() => {
+    timeleft--;
+    update_countdown();
+    
+    if(timeleft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeleft = 1500;
+      update_countdown();
+    }
+  }, 1000)
+};
+
+const pause_countdown = () => clearInterval(interval);
+
+button.addEventListener("click", start_countdown);
+stop.addEventListener("click", pause_countdown);
