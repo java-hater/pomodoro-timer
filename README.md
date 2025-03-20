4ed88d2f686d82f3ddf22ae241e23e219e3f5114
nik <nameissubjecttochange@gmail.com>
2025-03-20 07:15:19 -0400

    added alert sounds, debug availability, and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pomodoro-timer
- A pomodoro timer for personal use.
+The Pomodoro Technique (https://en.wikipedia.org/wiki/Pomodoro_Technique) is a technique characterized by four repetitions of a 25 minute focus time followed by a 5 minute break time. At the end of four repetitions, a 20 minute break follows before repeating the central loop.
+I chose not to have the timer automatically move from timer to timer in accordance with this technique, as its use case is more convenient with manual input.
--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const body = document.getElementById('body');
 const count_button = document.getElementsByClassName('count-button');
 const timer_button = document.getElementsByClassName('small-button');
 
-const colors = ["#086788", "#75BBA7", "#AA968A"];
-
 let running = false;
-
-let timeleft = 1500; //25 minutes
 let interval;
-let current_timer = 0; //0 = Pomodoro; 1 = Short Break; 2 = Long Break
+let current_timer = 0; //0 = Pomodoro; 1 = Short Break; 2 = Long Break; 3 = Debug;
+const colors = ["#086788", "#75BBA7", "#AA968A"];
+const times = [1500, 300, 1200, 10]; //20 minutes, 5 minutes, and 20 minutes respectively
+let timeleft = times[current_timer];
 
 const update_countdown = () => {
   const mins = Math.floor(timeleft / 60);
@@ -34,8 +33,8 @@ const start_countdown = () => {
     
     if(timeleft === 0) {
       clearInterval(interval);
-      alert("Time's up!");
-      timeleft = 1500;
+      play_sound();
+      timeleft = times[current_timer];
       running = false;
       update_countdown();
     }
@@ -47,6 +46,11 @@ const pause_countdown = () => {
   running = false;
 };
 
+function play_sound() {
+  var beep = new Audio('notification-alert.mp3');
+  beep.play();
+}
+
 function timer_handler(newtime) {
   clearInterval(interval);
   timeleft = newtime;
@@ -56,27 +60,32 @@ function timer_handler(newtime) {
   count_button[0].style.color = colors[current_timer];
   count_button[1].style.color = colors[current_timer];
   timer_button[current_timer].style.backgroundColor = "rgba(0,0,0,0.1)";
-  console.log(current_timer);
 }
 
 const focushandler = () => {
   timer_button[current_timer].style.backgroundColor = "rgba(0,0,0,0)";
   current_timer = 0;
-  timer_handler(1500);
+  timer_handler(times[current_timer]);
 };
 const shorthandler = () => {
   timer_button[current_timer].style.backgroundColor = "rgba(0,0,0,0)";
-  current_timer = 1
-  timer_handler(300);
+  current_timer = 1;
+  timer_handler(times[current_timer]);
 };
 const longhandler = () => {
   timer_button[current_timer].style.backgroundColor = "rgba(0,0,0,0)";
-  current_timer = 2
-  timer_handler(1200);
+  current_timer = 2;
+  timer_handler(times[current_timer]);
 };
+const debug = () => {
+  timer_button[current_timer].style.backgroundColor = "rgba(0,0,0,0)";
+  current_timer = 3;
+  timer_handler(times[current_timer]);
+}
 
 button.addEventListener("click", start_countdown);
 pause.addEventListener("click", pause_countdown);
 timer_button[0].addEventListener("click", focushandler);
 timer_button[1].addEventListener("click", shorthandler);
 timer_button[2].addEventListener("click", longhandler);
+timer_button[3].addEventListener("click", debug);
